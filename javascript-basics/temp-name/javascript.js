7d330032218e02ee2deef70aa403f23384a047ac
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-07-08 14:37:53 +0800

    Feat: try reassigning variables without let

--- javascript-basics/temp-name/javascript.js
+++ javascript-basics/temp-name/javascript.js
@@ -1,4 +1,13 @@
 let fruitRed = "Apple";
 let fruitYellow = "Banana";
+let fruits = 2;
 
 console.log(fruitRed, fruitYellow)
+console.log("Fruits:", fruits)
+
+let fruitPurple = "Grapes";
+fruitRed = "Strawberry";
+fruits = 3;
+
+console.log(fruitRed, fruitYellow, fruitPurple)
+console.log("Fruits:", fruits)

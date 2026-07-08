9bbd1e0d9b3a51fbd45f716d4659132bc64b20bd
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-07-08 15:35:54 +0800

    Feat: rename folder and try out assignment

---
+++ javascript-basics/variables-and-operators/javascript.js
@@ -0,0 +1,28 @@
+let fruitRed = "Apple"
+let fruitYellow = "Banana"
+let fruits = 2
+
+console.log(fruitRed, fruitYellow)
+console.log("Fruits:", fruits)
+
+let fruitPurple = "Grapes"
+fruitRed = "Strawberry"
+fruits = 3
+
+console.log(fruitRed, fruitYellow, fruitPurple)
+console.log("Fruits:", fruits)
+
+console.log(23 + 97)
+console.log(20 + 20 + 30 + 30 + 100 * 2)
+console.log((4 + 6 + 9) / 77)
+
+let a = 20
+console.log(a)
+a = 10
+let b = 7 * a
+console.log(b)
+
+const max = 57
+const actual = max - 13
+const percentage = actual / max
+console.log(percentage)

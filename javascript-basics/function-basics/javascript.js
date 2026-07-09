10507f43424a74ce9e3d1a8898cd47b47b564109
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-07-09 22:09:12 +0800

    Feat: solve assignment applying functions

---
+++ javascript-basics/function-basics/javascript.js
@@ -0,0 +1,19 @@
+function add7(number) {
+    return number + 7
+}
+console.log(add7(6))
+
+function multiply(x, y) {
+    return x * y
+}
+console.log(multiply(6, 7))
+
+function capitalize(text) {
+    return text[0].toUpperCase() + text.slice(1).toLowerCase()
+}
+console.log(capitalize("odIN"))
+
+function lastLetter(text) {
+    return text.slice(-1)
+}
+console.log(lastLetter("Project"))

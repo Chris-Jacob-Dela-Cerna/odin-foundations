72e0bb97ae1c5e95885abfcf30b9075639c5743d
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-07-13 21:00:06 +0800

    Feat: add red text paragraph

--- javascript-basics/dom-exercise/javascript.js
+++ javascript-basics/dom-exercise/javascript.js
@@ -3,5 +3,10 @@ const container = document.querySelector("#container");
 const content = document.createElement("div");
 content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
-
 container.appendChild(content);
+
+const redText = document.createElement("p");
+redText.style["color"] = "red"
+redText.textContent = "Hey I'm red!"
+container.appendChild(redText)
+

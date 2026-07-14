87855546cb13e1f4d81dd25282f8cc5c9026daef
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-07-14 15:44:15 +0800

    Feat: try out .onlick and addEventListener

---
+++ javascript-basics/dom-event-exercise/javascript.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector("#btn")
+btn.onclick = () => alert("Hello there!")
+
+const bttn = document.querySelector("#bttn")
+bttn.addEventListener("click", () => alert("Hello there!"))

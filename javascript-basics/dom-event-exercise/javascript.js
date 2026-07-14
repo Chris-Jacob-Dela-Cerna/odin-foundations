738ace2c61cbf58b2a5fc46556557279efaf8059
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-07-14 16:10:03 +0800

    Feat: try using event parameter to attach to the DOM

--- javascript-basics/dom-event-exercise/javascript.js
+++ javascript-basics/dom-event-exercise/javascript.js
@@ -1,5 +1,8 @@
 const btn = document.querySelector("#btn")
 btn.onclick = () => alert("Hello there!")
+btn.addEventListener("click", (e) => {
+  e.target.style.background = "blue"
+})
 
 const bttn = document.querySelector("#bttn")
 bttn.addEventListener("click", () => alert("Hello there!"))

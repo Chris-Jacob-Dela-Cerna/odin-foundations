52569920bd5c823e28b1e2be3ec67b36d240cbbf
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-07-24 14:53:23 +0800

    feat: map object names into full names

--- javascript-basics/object-exercise/script.js
+++ javascript-basics/object-exercise/script.js
@@ -4,5 +4,18 @@ let pete = {name: "Pete", age: 30};
 let mary = {name: "Mary", age: 28};
 let users = [john, pete, mary];
 
-let names = users.map(user => user.name)
+let names = users.map(user => user.name);
 console.log("1. " + names); // John, Pete, Mary
+
+
+
+let john2 = { name: "John", surname: "Smith", id: 1 };
+let pete2 = { name: "Pete", surname: "Hunt", id: 2 };
+let mary2 = { name: "Mary", surname: "Key", id: 3 };
+let users2 = [ john2, pete2, mary2 ];
+
+let usersMapped = users2.map(user => ({
+    name: `${user['name']} ${user['surname']}`,
+    id: user['id'],
+}));
+console.log(usersMapped);
